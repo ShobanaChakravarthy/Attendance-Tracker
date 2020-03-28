c9add0aa58bcd7c1db8a876447ec9caeae2f39a4
shocaskate <shobana.chakravarthy@gmail.com>
2020-03-28 10:33:30 +0530

    Second version

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import json
 import pandas
 import csv
 import datetime
+from pathlib import Path
+import os
+
 
 app=Flask(__name__)
 
@@ -57,6 +60,7 @@ def capture():
 @app.route('/display',methods=["POST"])
 def display():
     global filename
+    global filename1
     if request.method == "POST":
         try:
             scrumfet__ = request.form["scrumfet"]
@@ -69,7 +73,7 @@ def display():
                 'A': "0"        }
             r = requests.get('https://fsgrspskd7.execute-api.us-west-2.amazonaws.com/PostApiFinal/', json = PARAMS)
             #b=str([{"Id": "679882", "LoginTime": "0900", "LogoutTime": "1800", "Scrum": "scrum1"}, {"Id": "681472", "LoginTime": "0900", "LogoutTime": "1800", "Scrum": "scrum1"}])
-            b=r.text
+            #b=r.text
             b = b.replace("'", '"')
             a = json.loads(b)
             df=pandas.DataFrame()
@@ -89,10 +93,31 @@ def display():
                 j=j+1
             df.insert(0, "Employee Id", Employee_Id)
             df.insert(1, "Employee Name", Employee_Name)
-            df.insert(2, "Login Time", Login_Time)
-            df.insert(3, "Logout Time", Logout_Time)
+            df.insert(2, "Login_Time", Login_Time)
+            df.insert(3, "Logout_Time", Logout_Time)
             df.insert(4, "Scrum", Scrum) 
-            filename=datetime.datetime.now().strftime("%Y-%m-%d"+".csv")
+            # to convert login time and logout time into time format
+            #for login time
+            df["time"]=df["Login_Time"]
+            df.time = df.time.astype(str).astype(float).astype(int).astype(str)
+            df.time = df.time.str.zfill(4)
+            df["time"] = df.time.apply(lambda x: x[0:2] + ":" + x[2:4])
+            df["Login Time"] = df["time"]
+            del df["Login_Time"]
+            del df["time"]
+            #for logout time
+            df["timei"]=df["Logout_Time"]
+            df.timei = df.timei.astype(str).astype(float).astype(int).astype(str)
+            df.timei = df.timei.str.zfill(4)
+            df["timei"] = df.timei.apply(lambda x: x[0:2] + ":" + x[2:4])
+            df["Logout Time"] = df["timei"]
+            del df["Logout_Time"]
+            del df["timei"]
+            #re-ordering columns
+            df = df[['Employee Id','Employee Name','Login Time','Logout Time','Scrum']]
+            path_to_download_folder = str(os.path.join(Path.home(), "Downloads"))
+            filename=datetime.datetime.now().strftime(path_to_download_folder+"/%Y-%m-%d"+".csv")
+            filename1=datetime.datetime.now().strftime("%Y-%m-%d"+".csv")
             df.to_csv(filename,index=None)
             return render_template("fetch.html",column_names=df.columns.values, row_data=list(df.values.tolist()), btn='download.html')
         except Exception as e:
@@ -100,7 +125,7 @@ def display():
 
 @app.route("/download-file/")
 def download():
-    return send_file(filename, attachment_filename=filename, as_attachment=True)
+    return send_file(filename, attachment_filename=filename1, as_attachment=True)
 
 
 if __name__=='__main__':    
